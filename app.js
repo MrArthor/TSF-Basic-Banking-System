ba0302a285ba391ffd0be8ebae58a4640a5c926e
Vansh Sachdev <vksvs25@gmail.com>
2021-08-17 22:03:22 +0530

    Final Copy :)

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const User = require('./models/user');
-const Transaction = require('./models/transaction');
+const User = require('./Models/User');
+const Transaction = require('./Models/Transection');
 
 
 mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("Database Connected")
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log("Database Connection Error")
         console.log(err)
     })
 
@@ -57,11 +57,11 @@ app.put("/view/:id1/:id2", async(req, res) => {
         await User.findByIdAndUpdate(id1, { credits: fromCreditsNew }, { runValidators: true, new: true });
         await User.findByIdAndUpdate(id2, { credits: toCreditsNew }, { runValidators: true, new: true });
 
-        let newTransaction = new Transaction();
-        newTransaction.fromName = fromUser.name;
-        newTransaction.toName = toUser.name;
-        newTransaction.transfer = credit;
-        await newTransaction.save();
+        let NewTransection = new Transaction();
+        NewTransection.fromName = fromUser.name;
+        NewTransection.toName = toUser.name;
+        NewTransection.transfer = credit;
+        await NewTransection.save();
 
         res.redirect("/view");
     } else {
